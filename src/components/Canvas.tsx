c40c4057cb9da9fe1e86325226c23e035ce194b0
Michael <micjanic20@gmail.com>
2020-12-22 01:23:00 -0600

    added canvas

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,12 +1,25 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 
 interface CanvasProps {
   width: number;
   height: number;
 }
 
+type Coordinate = {
+  x: number;
+  y: number;
+}
+
 const Canvas = ({ width, height }: CanvasProps) => {
   const canvasRef = useRef<HTMLCanvasElement>(null)
+  const [digits, setDigits] = useState([])
+  const [mousePosition, setMousePosition] = useState<Coordinate | undefined>(undefined)
+
+  useEffect(() => {
+    if (!canvasRef.current) {
+      return
+    }
+  })
 
   return <canvas ref={canvasRef} height={height} width={width} />
 }
