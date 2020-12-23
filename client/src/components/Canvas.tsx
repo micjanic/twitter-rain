7daece506d27fab0b5fd069aea0e2b3d228ed040
Michael <micjanic20@gmail.com>
2020-12-22 22:54:30 -0600

    use const for sizing

--- client/src/components/Canvas.tsx
+++ client/src/components/Canvas.tsx
@@ -15,16 +15,19 @@ type Coordinate = {
 */
 const Canvas = ({ width, height }: CanvasProps) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const contextRef = useRef<CanvasRenderingContext2D>(null);
+  const ctxRef = useRef<CanvasRenderingContext2D>(null);
   //const [mousePosition, setMousePosition] = useState<Coordinate | undefined>(undefined)
 
-  const [columns, setColumns] = useState<number>();
+  const [columns, setColumns] = useState<number[]>();
   const [twitterMessages, setTwitterMessages] = useState([
     "The quick brown fox jumps over the lazy dog.",
     "Look out! There are llamas!",
     "No, really, don't get up.",
     "Howdy Ho Neightborino",
-    "Every day - if walking through the shops count as working out."
+    "Every day - if walking through the shops count as working out.",
+    "It is a fair, even-handed, noble adjustment of things, that while there is infection in disease and sorrow, there is nothing in the world so irresistibly contagious as laughter and good humour.",
+    "No space of regret can make amends for one lifes opportunity misused",
+    "You may be an undigested bit of beef, a blot of mustard, a crumb of cheese, a fragment of underdone potato. There's more of gravy than of grave about you, whatever you are!"
   ]);
 
 
@@ -34,7 +37,6 @@ const Canvas = ({ width, height }: CanvasProps) => {
     }
 
     const canvas = canvasRef.current;
-
     const w = canvas.width = document.body.offsetWidth;
     const h = canvas.height = document.body.offsetHeight;
 
@@ -47,14 +49,33 @@ const Canvas = ({ width, height }: CanvasProps) => {
     ctx.scale(2, 2);
     ctx.fillStyle = '#000'
     ctx.fillRect(0, 0, w, h);
+    ctxRef.current = ctx;
 
-    contextRef.current = ctx;
-    setColumns(window.innerWidth / FONT_SIZE)
+    setColumns(Array(Math.floor(w / FONT_SIZE) + 1).fill(0));
   }, [])
 
+  const rain = () => {
+    const w = document.body.offsetWidth;
+    const h = document.body.offsetHeight;
+
+    ctxRef.current.fillStyle = '#0001';
+    ctxRef.current.fillRect(0, 0, w, h);
+
+    ctxRef.current.fillStyle = '#0f0';
+    ctxRef.current.font = `${FONT_SIZE}pt monospace`;
+
+    columns.forEach((y, ind) => {
+      const text = String.fromCharCode(Math.random() * 128);
+      const x = ind * FONT_SIZE;
+      ctxRef.current.fillText(text, x, y);
+      if (y > 100 + Math.random() * 10000) columns[ind] = 0;
+      else columns[ind] = y + FONT_SIZE;
+    });
+  }
+
   useInterval(() => {
-    //console.log('tick');
-  }, 1000)
+    rain()
+  }, SPEED)
 
   return <canvas ref={canvasRef} height={height} width={width} />
 }
--- client/src/components/constants/Constants.ts
+++ client/src/components/constants/Constants.ts
@@ -1,6 +1,6 @@
 export const MAX_MESSAGES: number = 10240;
-export const FONT_SIZE: number = 16;
-export const SPEED: number = 44;
+export const FONT_SIZE: number = 12;
+export const SPEED: number = 75;
 
 
 export const spawnInterval = 500;
